cf8e5d29f15e78668f2c0025c89aafdcb31ae5f0
s-A301-27 <s-A301-27@K-A301-27>
2025-03-03 19:40:13 +0100

    Dodaj pliki projektów.

---
+++ EShopSErvice/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace EShopSErvice.Models
+{
+    public class Category : BaseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ EShopSErvice/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace EShopSErvice.Models
+{
+    public class Product : BaseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public string ean { get; set; } = default!;
+
+        public decimal price { get; set; }
+
+        public int stock { get; set; } = 0;
+
+        public string sku { get; set; } = default!;
+
+        public Category? Category { get; set; }
+
+    }
+}
